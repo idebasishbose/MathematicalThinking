9dfd2b091be6b33ca3f82703083b56d0aaf4ad40
Debasish Bose <devbose2008@gmail.com>
2023-09-05 21:52:01 -0400

    edit

--- nqueen.py
+++ nqueen.py
@@ -5,7 +5,7 @@ from rich import print
 
 def is_solution(perm: list):
     for i1, i2 in it.combinations(range(len(perm)), 2):
-        if abs(i1 - i2) == abs(perm[i1] - perm[i1]):
+        if abs(i1 - i2) == abs(perm[i1] - perm[i2]):
             return False
     return True
 
--- test_nqueen.py
+++ test_nqueen.py
@@ -3,5 +3,5 @@ from nqueen import is_solution
 
 def test_is_solution():
     assert is_solution([1, 3, 0, 2]) == True
-    assert is_solution([0, 1, 2, 3, 4, 5, 6, 7]) == True
+    assert is_solution([0, 4, 7, 5, 2, 6, 1, 3]) == True
     assert is_solution([3, 1, 0, 2]) == False
